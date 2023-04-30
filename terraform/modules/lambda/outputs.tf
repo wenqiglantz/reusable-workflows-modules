609e1d1ef3ef068aa0796e9c20e7a20913b8124d
wenqiglantz <wenqiglantz@gmail.com>
2023-04-30 00:44:44 -0400

    revised output

--- terraform/modules/lambda/outputs.tf
+++ terraform/modules/lambda/outputs.tf
@@ -1,3 +1,7 @@
+output "lambda_function_arn" {
+  value = module.lambda_function
+}
+
 output "lambda_name" {
   value = {for k, v in module.lambda_function: k => v.function_name}
   description = "Lambda function names in key/value map"
